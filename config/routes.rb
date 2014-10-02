745259433ca85605c606661d1d158a1e720efe57
mandym_webdev <mandy@mandym.com>
2014-10-02 15:05:21 -0400

    deleted duplicate migration and added routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,15 @@
 Rails.application.routes.draw do
   devise_for :users
+
+  root 'posts#index'
+
+  resources :users do
+    resources :follows
+  end
+
+  
+
+
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
