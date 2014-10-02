6bda5ae5c4785966faca4513d32767ad98ddb78e
brandonjyuhas <brandonjyuhas@gmail.com>
2014-10-02 15:00:26 -0400

    createdControllers

---
+++ app/controllers/users_controller.rb
@@ -0,0 +1,33 @@
+class UsersController < ApplicationController
+
+	def index
+		@users = User.all.order(:name)
+	end
+
+	def show
+		@user = User.find(params[:id])
+		@posts = Posts.where(user_id: params[:id])
+	end
+
+	def edit
+		@user = User.find(params[:id])
+	end
+
+	def update
+		@user = User.find(params[:id])
+		@user.update(user_params)
+	end
+
+	def delete
+		@user = User.find(params[:id])
+		@posts = Post.where(user_id: params[:id])
+		@user.destroy
+		@posts.delete
+	end
+
+	private
+
+		def user_params
+			params.require(:user).permit(:username, :bio, :password, :password_confirmation, :link, :profile_pic)
+		end
+end
