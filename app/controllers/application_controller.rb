e9204c7199a36413342d42ab543ee4f927d681ba
brandonjyuhas <brandonjyuhas@gmail.com>
2014-10-03 10:21:47 -0400

    Move devise sanitizer to registration_controller.rb

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,16 +3,6 @@ class ApplicationController < ActionController::Base
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
 
-  protected 
 
-  def configure_permitted_parameters
-  	devise_parameter_sanitizer.for(:sign_up) do |user|
-  		user.permit(:username, :bio, :personal_website, :email, :password, :password_confirmation, :profile_photo)
-  	end 
-
-  	devise_parameter_sanitizer.for(:account_update) do |user|
-  		user.permit(:username, :bio, :personal_website, :email, :password, :password_confirmation, :profile_photo)
-  	end 
-  end
 
 end
