5c7a6931398daee463c3ca92118a321973da7d5c
brandonjyuhas <brandonjyuhas@gmail.com>
2014-10-03 14:58:05 -0400

    after lunch push2

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -6,6 +6,8 @@ class PostsController < ApplicationController
 		@followed_ids = Follow.where(follower_id: current_user.id).select(:followed_id).map(&:followed_id)
 
 		@posts = Post.where(user_id: @followed_ids)
+		# @posts << Post.where(user_id:current_user.id)
+		@posts.order(:created_at)
 	end
 
 	def show
@@ -14,6 +16,13 @@ class PostsController < ApplicationController
 
 	def create
 		@post = Post.new(post_params)
+
+		    if @post.save
+		    	puts "\n\n\n\n\n\n #{@post} \n\n\n\n"
+      render json: @post
+    else
+      render status: 400, nothing: true
+    end
 	end
 
 	def delete
