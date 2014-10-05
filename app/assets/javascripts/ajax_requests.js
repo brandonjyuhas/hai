d637347b392d30b05d58e7f88703beb19974db84
pam- <pam.assogba@gmail.com>
2014-10-05 18:05:36 -0400

    ajax works for bio

--- app/assets/javascripts/ajax_requests.js
+++ app/assets/javascripts/ajax_requests.js
@@ -4,7 +4,21 @@ var ajaxEdits = function(){
 		$('#about input[type="text"]').keypress(function(event){
 		if (event.which === 13) {
 			event.preventDefault();
-			alert('here')
+			var bio = $(this).val();
+			var user_id = $('#about').data('id');
+			$.ajax({
+				type: 'PATCH',
+				url: user_id,
+				dataType: 'json',
+				data: {
+					user: {
+						bio: bio
+					}			
+				},
+				success: function(){
+					$('#about').html('<h4><small>'+ bio +'</small></h4>');
+				}
+			})
 		};
 	})
 }
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -18,6 +18,13 @@ class UsersController < ApplicationController
 	def update
 		@user = User.find(params[:id])
 		@user.update(user_params)
+
+		if @user.save
+			respond_to do |format|
+				format.html { redirect_to @user  }
+				format.json { render json: @user, status: 200 }
+			end
+		end
 	end
 
 	def delete
@@ -25,5 +32,11 @@ class UsersController < ApplicationController
 		@posts = Post.where(user_id: params[:id])
 		@user.destroy
 		@posts.delete
+	end
+
+	private 
+
+	def user_params
+		params.require(:user).permit(:username, :profile_photo, :email, :password, :password_confirmation, :bio, :personal_website)
 	end
 end
